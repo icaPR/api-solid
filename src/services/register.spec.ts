f8feade0760b7aa369a00f2e9515170f1992b15f
Icaro Ramos <polosnramos@hotmail.com>
2024-05-12 23:04:01 -0300

    feat: Add unit test to prevent creation of two users with the same email

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -18,4 +18,22 @@ describe("Register services", () => {
 
     expect(isPasswordHashCorrectly).toBe(true);
   });
+  it("should not be possible to register two identical emails", async () => {
+    const inMemoryUsersRepository = new InMemoryUsersRepository();
+    const registerService = new RegisterService(inMemoryUsersRepository);
+
+    await registerService.handleRegister({
+      name: "test",
+      email: "test@email.com",
+      password: "123456",
+    });
+
+    expect(() =>
+      registerService.handleRegister({
+        name: "test",
+        email: "test@email.com",
+        password: "123456",
+      })
+    ).rejects.toBeInstanceOf(UserAlreadyExists);
+  });
 });
