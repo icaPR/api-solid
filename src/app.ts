e65db9363ad34a01d9cf6f36bc8e5a1b63644b9f
Icaro Ramos <polosnramos@hotmail.com>
2024-05-12 21:37:46 -0300

    feat: Add error handling to app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,23 @@
 import fastify from "fastify";
 import { appRoutes } from "./http/routes";
+import { ZodError } from "zod";
+import { env } from "./env";
 
 export const app = fastify();
 
 app.register(appRoutes);
+
+app.setErrorHandler((e, req, res) => {
+  if (e instanceof ZodError) {
+    return res
+      .status(404)
+      .send({ message: "Validatrion error.", issues: e.format() });
+  }
+
+  if (env.NODE_ENV !== "production") {
+    console.error(e);
+  } else {
+  }
+
+  return res.status(500).send({ message: "Internal server error" });
+});
