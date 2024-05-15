91b28da945e161729cb6335835558ce6b101c529
Icaro Ramos <polosnramos@hotmail.com>
2024-05-15 16:21:00 -0300

    feat: Add searchMany and findManyNearby methods to gyms repository

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,6 +1,7 @@
 import { Prisma, Gym } from "@prisma/client";
-import { GymsRepository } from "../gyms-repository";
+import { FindManyNearbyProps, GymsRepository } from "../gyms-repository";
 import { randomUUID } from "crypto";
+import { getDistanceBetweenCoord } from "@/utils/get-distance-between-coord";
 
 export class InMemoryGymsRepository implements GymsRepository {
   public items: Gym[] = [];
@@ -14,14 +15,34 @@ export class InMemoryGymsRepository implements GymsRepository {
     return gym;
   }
 
+  async searchMany(query: string, page: number) {
+    return this.items
+      .filter((item) => item.name.includes(query))
+      .slice(page * 20 - 20, page * 20);
+  }
+
+  async findManyNearby(params: FindManyNearbyProps) {
+    return this.items.filter((item) => {
+      const distance = getDistanceBetweenCoord(
+        { latitude: params.latitude, longitude: params.longitude },
+        {
+          latitude: item.latitude.toNumber(),
+          longitude: item.longitude.toNumber(),
+        }
+      );
+
+      return distance < 10;
+    });
+  }
+
   async create(data: Prisma.GymCreateInput) {
     const gym = {
       id: data.id ?? randomUUID(),
       name: data.name,
       description: data.description ?? null,
       phone: data.phone ?? null,
-      latitude: new Prisma.Decimal(data.latitude.toLocaleString()),
-      longitude: new Prisma.Decimal(data.longitude.toLocaleString()),
+      latitude: new Prisma.Decimal(data.latitude.toString()),
+      longitude: new Prisma.Decimal(data.longitude.toString()),
     };
 
     this.items.push(gym);
