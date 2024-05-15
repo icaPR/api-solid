35b2a7210876abda7b6345e749b8f9159907745f
Icaro Ramos <polosnramos@hotmail.com>
2024-05-15 17:17:29 -0300

    feat: Add caountByUserId, save, findById and findManyByUserId methods to check-ins repository

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -2,8 +2,12 @@ import { Prisma, CheckIn } from "@prisma/client";
 
 export interface CheckInsRepository {
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>;
+  save(checkIn: CheckIn): Promise<CheckIn>;
   findByUserIdOnDateCheckin(
     userId: string,
     date: Date
   ): Promise<CheckIn | null>;
+  findManyByUserId(userId: string, page: number): Promise<CheckIn[]>;
+  findById(checkInID: string): Promise<CheckIn | null>;
+  caountByUserId(userId: string): Promise<number>;
 }
--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -6,6 +6,14 @@ import dayjs from "dayjs";
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = [];
 
+  async findById(checkInID: string) {
+    const checkIn = this.items.find((item) => item.id === checkInID);
+    if (!checkIn) {
+      return null;
+    }
+    return checkIn;
+  }
+
   async findByUserIdOnDateCheckin(userId: string, date: Date) {
     const startOfTheDay = dayjs(date).startOf("date");
     const endOfTheDay = dayjs(date).endOf("date");
@@ -27,6 +35,16 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return checkInOnSameDate;
   }
 
+  async findManyByUserId(userId: string, page: number) {
+    return this.items
+      .filter((item) => item.user_id === userId)
+      .slice(page * 20 - 20, page * 20);
+  }
+
+  async caountByUserId(userId: string) {
+    return this.items.filter((item) => item.user_id === userId).length;
+  }
+
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
       id: randomUUID(),
@@ -37,6 +55,17 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     };
 
     this.items.push(checkIn);
+    return checkIn;
+  }
+
+  async save(checkIn: CheckIn) {
+    const checkInIndex = await this.items.findIndex(
+      (item) => item.id === checkIn.id
+    );
+
+    if (checkInIndex >= 0) {
+      this.items[checkInIndex] = checkIn;
+    }
     return checkIn;
   }
 }
