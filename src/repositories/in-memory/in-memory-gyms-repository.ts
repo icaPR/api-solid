e5627059514666be84441cc26c4056e9fd94e13c
Icaro Ramos <polosnramos@hotmail.com>
2024-05-14 21:18:45 -0300

    feat: Create gyms repository

---
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -0,0 +1,30 @@
+import { Prisma, Gym } from "@prisma/client";
+import { GymsRepository } from "../gyms-repository";
+import { randomUUID } from "crypto";
+
+export class InMemoryGymsRepository implements GymsRepository {
+  public items: Gym[] = [];
+
+  async findById(id: string) {
+    const gym = this.items.find((item) => item.id === id);
+
+    if (!gym) {
+      return null;
+    }
+    return gym;
+  }
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym = {
+      id: data.id ?? randomUUID(),
+      name: data.name,
+      description: data.description ?? null,
+      phone: data.phone ?? null,
+      latitude: new Prisma.Decimal(data.latitude.toLocaleString()),
+      longitude: new Prisma.Decimal(data.longitude.toLocaleString()),
+    };
+
+    this.items.push(gym);
+    return gym;
+  }
+}
