09fb0f0a48abcddb1a290d80a891fc0de047828c
Icaro Ramos <polosnramos@hotmail.com>
2024-05-15 16:16:41 -0300

    chore: Fix test for create-gym service

--- src/services/create-gym.spec.ts
+++ src/services/create-gym.spec.ts
@@ -1,26 +1,25 @@
 import { beforeEach, describe, expect, it } from "vitest";
-import { RegisterService } from "./register";
-import { compare } from "bcryptjs";
-import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
-import { UserAlreadyExists } from "./erros/user-already-exists";
+import { InMemoryGymsRepository } from "@/repositories/in-memory/in-memory-gyms-repository";
+import { CreateGymService } from "./create-gym";
 
-let inMemoryUsersRepository: InMemoryUsersRepository;
-let registerService: RegisterService;
+let inMemoryGymsRepository: InMemoryGymsRepository;
+let createGymService: CreateGymService;
 
 describe("Create gym services", () => {
   beforeEach(() => {
-    inMemoryUsersRepository = new InMemoryUsersRepository();
-    registerService = new RegisterService(inMemoryUsersRepository);
+    inMemoryGymsRepository = new InMemoryGymsRepository();
+    createGymService = new CreateGymService(inMemoryGymsRepository);
   });
 
-  it("user password must be hashed", async () => {
-    const { user } = await registerService.handleRegister({
+  it("should be able to create gym", async () => {
+    const { gym } = await createGymService.handleCreateGym({
       name: "test",
-      email: "test@email.com",
-      password: "123456",
+      description: null,
+      phone: null,
+      latitude: 0,
+      longitude: 0,
     });
-    const isPasswordHashCorrectly = await compare("123456", user.password_hash);
 
-    expect(isPasswordHashCorrectly).toBe(true);
+    expect(gym.id).toEqual(expect.any(String));
   });
 });
