74ba4842b582ada2abe0dcdcd469f96739dfa5cd
Icaro Ramos <polosnramos@hotmail.com>
2024-05-19 21:10:50 -0300

    feat: Verify user roles in routes

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -4,12 +4,17 @@ import { Create } from "./create";
 import { History } from "./history";
 import { Metrics } from "./metrics";
 import { Validate } from "./validate";
+import { verifyUserRole } from "@/http/middlewares/vertify-user-role";
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", VerifyJWT);
 
   app.post("/gyms/:gymId/check-ins", Create);
-  app.patch("/check-ins/:checkInId/validate", Validate);
+  app.patch(
+    "/check-ins/:checkInId/validate",
+    { onRequest: [verifyUserRole("ADMIN")] },
+    Validate
+  );
 
   app.get("/check-ins/history", History);
   app.get("/check-ins/metrics", Metrics);
--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -3,11 +3,12 @@ import { VerifyJWT } from "../../middlewares/verify-jwt";
 import { Search } from "./search";
 import { Nearby } from "./nearby";
 import { Create } from "./create";
+import { verifyUserRole } from "@/http/middlewares/vertify-user-role";
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", VerifyJWT);
 
-  app.post("/gyms", Create);
+  app.post("/gyms", { onRequest: [verifyUserRole("ADMIN")] }, Create);
 
   app.get("/gyms/search", Search);
   app.get("/gyms/nearby", Nearby);
